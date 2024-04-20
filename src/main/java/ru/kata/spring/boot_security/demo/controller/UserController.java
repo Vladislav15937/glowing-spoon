b53870169d1699138e6c08b4870fc2d4ed239559
Vladislav15937 <injewatkin.vlad@yandex.ru>
2024-04-21 04:25:23 +0700

    new
    new

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,69 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entity.Role;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    private final RoleService roleService;
+
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getUserList() {
+        List<User> userList = userService.show();
+        return userList == null && userList.isEmpty() ?
+                new ResponseEntity<>(userList, HttpStatus.NOT_FOUND) :
+                new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/roles")
+    public ResponseEntity<List<Role>> getRolesList() {
+        List<Role> roleSet = roleService.getAllRoles();
+        return roleSet == null && roleSet.isEmpty() ?
+                new ResponseEntity<>(roleSet, HttpStatus.NOT_FOUND) :
+                new ResponseEntity<>(roleSet, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        final User user = userService.getUserById(id).get();
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(value = "/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody @Valid User user) {
+        userService.save(user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/update")
+    public ResponseEntity<HttpStatus> update(@RequestBody @Valid User user) {
+        Long x = user.getId();
+        userService.updateById(x, user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
